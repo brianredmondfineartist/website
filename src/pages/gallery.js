78b9ae1b0498f929ff01538a1fec5b55b61f0ff2
brianredmondfineartist <59392083+brianredmondfineartist@users.noreply.github.com>
2026-09-24 15:54:43 +0100

    Update gallery.js

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -51,7 +51,7 @@ const Gallery = () => {
         <div className='image'>
           <figure>
             <div className='frame'>
-      <img src={`/website/gallery/${image.fields.slug}/${image.frontmatter.filename}`} alt={image.title} />
+<img src={`/website${image.fields.slug}/${image.frontmatter.filename}`} alt={image.title} />
             </div>
             <figcaption>
               <dl>
