a6b372ffcc26431776dc08ba50ab8d1da1a555b6
brianredmondfineartist <59392083+brianredmondfineartist@users.noreply.github.com>
2026-09-24 16:02:12 +0100

    Update gallery.js

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -18,6 +18,11 @@ const Gallery = () => {
               dimensions
               filename 
               slug
+              parent {
+  ... on File {
+    publicURL
+  }
+}
 
               
             }
